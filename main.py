210191d72fd47fb3a5386cfbef5d32799afeb82b
pateer1895 <pateer1895@gmail.com>
2026-06-06 23:36:26 +0200

    main.py

--- main.py
+++ main.py
@@ -12,15 +12,17 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 HORA_MESSAGE_ID = None
 HORARIOS_CHANNEL_NAME = "horarios"
 
+# estructura: { "mañana": set(), "tarde": set(), "noche": set() }
+horarios = {
+    "🌅": set(),
+    "🌇": set(),
+    "🌙": set()
+}
+
 
 @bot.event
 async def on_ready():
     print(f"Bot conectado como {bot.user}")
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong! 🏓")
 
 
 @bot.command()
@@ -32,22 +34,22 @@ async def crear_horarios(ctx):
         return
 
     msg = await ctx.send(
-        "🗓️ **Sistema de horarios**\n\n"
-        "Reacciona:\n"
-        "🟢 Disponible\n"
-        "🔴 No disponible"
+        "🗓️ **Horarios de quedada**\n\n"
+        "Reacciona según disponibilidad:\n\n"
+        "🌅 Mañana\n"
+        "🌇 Tarde\n"
+        "🌙 Noche"
     )
 
-    await msg.add_reaction("🟢")
-    await msg.add_reaction("🔴")
+    await msg.add_reaction("🌅")
+    await msg.add_reaction("🌇")
+    await msg.add_reaction("🌙")
 
     HORA_MESSAGE_ID = msg.id
 
 
 @bot.event
 async def on_reaction_add(reaction, user):
-    global HORA_MESSAGE_ID
-
     if user.bot:
         return
 
@@ -57,11 +59,32 @@ async def on_reaction_add(reaction, user):
     if reaction.message.id != HORA_MESSAGE_ID:
         return
 
-    if reaction.emoji == "🟢":
-        print(f"{user} está disponible")
+    emoji = reaction.emoji
 
-    elif reaction.emoji == "🔴":
-        print(f"{user} NO está disponible")
+    if emoji in horarios:
+        horarios[emoji].add(user.name)
+
+        print("----- HORARIOS -----")
+        print("🌅 Mañana:", horarios["🌅"])
+        print("🌇 Tarde:", horarios["🌇"])
+        print("🌙 Noche:", horarios["🌙"])
+
+
+@bot.event
+async def on_reaction_remove(reaction, user):
+    if user.bot:
+        return
+
+    if HORA_MESSAGE_ID is None:
+        return
+
+    if reaction.message.id != HORA_MESSAGE_ID:
+        return
+
+    emoji = reaction.emoji
+
+    if emoji in horarios and user.name in horarios[emoji]:
+        horarios[emoji].remove(user.name)
 
 
 bot.run(os.environ["TOKEN"])
