cb1a0979a467d4bc8409bed3c76603df2ff826de
pateer1895 <pateer1895@gmail.com>
2026-06-06 23:31:52 +0200

    main.py

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ async def on_ready():
     print(f"Bot conectado como {bot.user}")
 
 @bot.command()
-async def ping(ctx):
+ async def ping(ctx):
     await ctx.send("Pong! 🏓")
 
-bot.run(os.environ["TOKEN"])
-HORA_MESSAGE_ID = None
-HORARIOS_CHANNEL_NAME = "horarios"
+ bot.run(os.environ["TOKEN"])
+ HORA_MESSAGE_ID = None
+ HORARIOS_CHANNEL_NAME = "horarios"
 @bot.command()
-async def crear_horarios(ctx):
-    global HORA_MESSAGE_ID
+ async def crear_horarios(ctx):
+     global HORA_MESSAGE_ID
 
-    if ctx.channel.name != HORARIOS_CHANNEL_NAME:
+     if ctx.channel.name != HORARIOS_CHANNEL_NAME:
         await ctx.send("Este comando solo se usa en #horarios")
         return
 
-    msg = await ctx.send(
+     msg = await ctx.send(
         "🗓️ **Sistema de horarios**\n\n"
         "Reacciona:\n"
         "🟢 Disponible\n"
@@ -39,7 +39,7 @@ async def crear_horarios(ctx):
 
     HORA_MESSAGE_ID = msg.id
     @bot.event
-async def on_reaction_add(reaction, user):
+ async def on_reaction_add(reaction, user):
     if user.bot:
         return
 
