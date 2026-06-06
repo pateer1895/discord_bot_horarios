4b635ace568ead9c7cf0816211b22c19f09f27ab
pateer1895 <pateer1895@gmail.com>
2026-06-06 22:38:41 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bot conectado como {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! 🏓")
+
+bot.run(os.environ["TOKEN"])
