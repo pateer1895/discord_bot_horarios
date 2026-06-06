0fabc55d9bf59a3f2a4ae5583571d743cd3a099b
pateer1895 <pateer1895@gmail.com>
2026-06-06 23:32:44 +0200

    main.py

--- main.py
+++ main.py
@@ -5,29 +5,33 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.reactions = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+HORA_MESSAGE_ID = None
+HORARIOS_CHANNEL_NAME = "horarios"
+
 
 @bot.event
 async def on_ready():
     print(f"Bot conectado como {bot.user}")
 
+
 @bot.command()
- async def ping(ctx):
+async def ping(ctx):
     await ctx.send("Pong! 🏓")
 
- bot.run(os.environ["TOKEN"])
- HORA_MESSAGE_ID = None
- HORARIOS_CHANNEL_NAME = "horarios"
-@bot.command()
- async def crear_horarios(ctx):
-     global HORA_MESSAGE_ID
 
-     if ctx.channel.name != HORARIOS_CHANNEL_NAME:
+@bot.command()
+async def crear_horarios(ctx):
+    global HORA_MESSAGE_ID
+
+    if ctx.channel.name != HORARIOS_CHANNEL_NAME:
         await ctx.send("Este comando solo se usa en #horarios")
         return
 
-     msg = await ctx.send(
+    msg = await ctx.send(
         "🗓️ **Sistema de horarios**\n\n"
         "Reacciona:\n"
         "🟢 Disponible\n"
@@ -38,9 +42,16 @@ async def on_ready():
     await msg.add_reaction("🔴")
 
     HORA_MESSAGE_ID = msg.id
-    @bot.event
- async def on_reaction_add(reaction, user):
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    global HORA_MESSAGE_ID
+
     if user.bot:
+        return
+
+    if HORA_MESSAGE_ID is None:
         return
 
     if reaction.message.id != HORA_MESSAGE_ID:
@@ -51,3 +62,6 @@ async def on_ready():
 
     elif reaction.emoji == "🔴":
         print(f"{user} NO está disponible")
+
+
+bot.run(os.environ["TOKEN"])
